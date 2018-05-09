1cf36c966ac67b591837ab41707f3931f6beb028
John Vollmer <vollme22@uwm.edu>
2018-05-09 10:53:57 -0500

    adding pay calculator

---
+++ app.js
@@ -0,0 +1,24 @@
+"use strict";
+let payPrompt = prompt("Please enter your pay per hour", 0);
+let hoursPrompt = prompt("Please enter hours");
+hoursPrompt = parseFloat(hoursPrompt);
+console.log("Hourly wage: $" + payPrompt);
+console.log("Hours worked: " + hoursPrompt);
+let wage = (payPrompt * hoursPrompt);
+console.log("Pay: $" + wage);
+wage = wage.toFixed(2);
+console.log("Pay Rounded: $" + wage);
+let tax;
+if (wage <= 800)
+{
+tax = (wage * .125);
+}
+else
+{
+tax = (wage * .175);
+}
+let taxFixed = tax.toFixed(2);
+let wageAfterTax = (wage - taxFixed);
+wageAfterTax = wageAfterTax.toFixed(2);
+console.log("Taxes: $" + taxFixed);
+console.log("Pay After Taxes: $" + wageAfterTax);
