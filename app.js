996378753e6ff6b9984cfeb402c3129509d5e5f3
John Vollmer <vollme22@uwm.edu>
2018-05-09 15:50:09 -0500

    updating pay calculator

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 "use strict";
 let payPrompt = prompt("Please enter your pay per hour", 0);
-let hoursPrompt = prompt("Please enter hours");
-hoursPrompt = parseFloat(hoursPrompt);
-console.log("Hourly wage: $" + payPrompt);
-console.log("Hours worked: " + hoursPrompt);
+let hoursPrompt = prompt("Please enter hours", 0);
+let wage;
+if (hoursPrompt > 40)
+{
+	let hoursOT = hoursPrompt - 40;
+	let payOT = payPrompt * 1.5;
+	wage = ((40 * payPrompt) + (hoursOT * payOT));
+	console.log("Hourly wage: $" + payPrompt);
+	console.log("Hours worked: " + hoursPrompt);
+	console.log("Hours worked OT: " + hoursOT)
+}
+else
+{
+	wage = (hoursPrompt * payPrompt);
+	console.log("Hourly wage: $" + payPrompt);
+	console.log("Hours worked: " + hoursPrompt);
+}
+/*<hoursPrompt = parseFloat(hoursPrompt);*/
+console.log("Weekly wage: $" + wage);
+/*
 let wage = (payPrompt * hoursPrompt);
 console.log("Pay: $" + wage);
 wage = wage.toFixed(2);
@@ -24,4 +40,4 @@ let wageAfterTax = (wage - taxFixed);
 wageAfterTax = wageAfterTax.toFixed(2);
 console.log("Taxes: $" + taxFixed);
 console.log("Pay After Taxes: $" + wageAfterTax);
-alert("Pay After Taxes: $" + wageAfterTax);
+alert("Pay After Taxes: $" + wageAfterTax);*/
